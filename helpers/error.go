6df0b98a8f619d70cfaee98b1ce0679052f0ca01
Sina Meshkini <meshkini.dev@gmail.com>
2022-12-28 00:14:44 +0330

    add error

--- helpers/error.go
+++ helpers/error.go
@@ -3,6 +3,8 @@ package helpers
 import "errors"
 
 var (
+	ErrAlreadyExist    = errors.New("already exist")
+	ErrNotFound        = errors.New("not found")
 	ErrRecordNotFound  = errors.New("record not found")
 	ErrInvalidRequest  = errors.New("invalid request data")
 	ErrEmailIsVerified = errors.New("email is verified")
