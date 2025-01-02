f43d3e643cff3409bb730733675867d4a4016710
Sina <meshkini.dev@gmail.com>
2025-01-02 21:29:18 +0330

    fix json tags

--- models/model.go
+++ models/model.go
@@ -10,29 +10,29 @@ import (
 type ModelTime struct {
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
 }
 
 // ModelIID basic model with auto incremental id
 type ModelIID struct {
-	ID IID `gorm:"primarykey"`
+	ID IID `gorm:"primarykey" json:"id"`
 	ModelTime
 }
 
 // ModelSID basic model with snowflake id
 type ModelSID struct {
-	ID SID
+	ID SID `gorm:"primarykey" json:"id"`
 	ModelTime
 }
 
 // ModelUUID basic model with UUID
 type ModelUUID struct {
-	ID uuid.UUID `gorm:"primarykey"`
+	ID uuid.UUID `gorm:"primarykey" json:"id"`
 	ModelTime
 }
 
 // Model basic model with string id
 type Model struct {
-	ID string `gorm:"primarykey"`
+	ID string `gorm:"primarykey" json:"id"`
 	ModelTime
 }
